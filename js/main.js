9d83fdb5334c89f4427c99541683ad57c2c40bd8
mspolermo <mspolermo@gmail.com>
2023-06-13 17:29:37 +0500

    tap-clicker added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
     },
 });
 
@@ -9,3 +11,19 @@ const swiper = new Swiper('.swiper', {
 $( function() {
     $( "#accordion" ).accordion({collapsible: true, active: false});
 } );
+//console.log(document.querySelectorAll('.section-working__link'))
+document.querySelectorAll('.section-working__link').forEach(function(tubsBtn){
+  tubsBtn.addEventListener('click', function(event){
+    const path = event.currentTarget.dataset.path
+
+    document.querySelectorAll('.section-working__link').forEach(function(tabContent){
+      tabContent.classList.remove('section-working__link_active')
+    })
+    document.querySelector(`[data-path="${path}"]`).classList.add('section-working__link_active')
+
+    document.querySelectorAll('.section-working__left').forEach(function(tabContent){
+      tabContent.classList.remove('section-working__left_active')
+    })
+    document.querySelector(`[data-target="${path}"]`).classList.add('section-working__left_active')
+  })
+})
